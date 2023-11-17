6592cc2b315eba7251c25e2cf9d9fccf2ffa1b9f
it@westerwald.yoga-vidya.de <it@westerwald.yoga-vidya.de>
2023-11-17 16:56:28 +0100

    Autocomment: Adding files from PC: WW-Akama, User: damodara On 2023-11-17, at 16:56:28

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Quellcode automatisch über inotify und Ansible auf Anderen Geräten Deployen
 
-Vorrausetzungen, installiert:
+* Vorrausetzungen, installiert:
+  * GNU find
   * yq [NICHT jq! das ist für json, yq ist yaml]
   * intoifywait
   * ansible
--- install.sh
+++ install.sh
@@ -3,30 +3,30 @@
 # Ermittle das Verzeichnis, in dem das Skript liegt
 # Verwende "readlink" für absolute Pfade und "dirname" für relative Pfade
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR" || exit 1
 
 bins="deploycode deploycode-test"
 folders="/etc/deploycode/configs-available /etc/deploycode/configs-enabled /etc/deploycode/playbooks /etc/deploycode/playbook-vars"
 
 echo Checking Foldrs...
 for folder in $folders; do
-    echo Folder: $folder
-    [ ! -d "$folder" ] && mkdir -p $folder
+	echo Folder: "$folder"
+	[ ! -d "$folder" ] && mkdir -p "$folder"
 done
 
 echo "Installig Mainscript..."
 for bin in $bins; do
-    cp -fvu $bin /usr/bin
+	cp -fvu "$bin" /usr/bin
 done
 
 echo Installing Libaries..
 cp -fvu usr/lib/libDeploy /usr/lib
 
-for file in $(ls ./etc/deploycode/playbooks); do
-    [ -f "./etc/deploycode/playbooks/$file" ] && {
-        echo file: $file
-        cp -rv ./etc/deploycode/playbooks/$file /etc/deploycode/playbooks
-    }
+for file in ./etc/deploycode/playbooks/*; do
+	[ ! -r "$file" ] && continue
+
+	echo file: "$file"
+	cp -rv "$file" /etc/deploycode/playbooks
 done
 
 echo Copying Service Skript...
